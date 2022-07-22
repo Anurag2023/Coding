d7d10c78ad224b8cb4a9ef59b38d585276f9693b
Anurag Choudhary <panigale215@gmail.com>
2022-07-23 02:24:13 +0530

    bit_manipulation

---
+++ bit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n,i=0,ans=0;
+    cout<<"enter a no : ";
+    cin>>n;
+    while(n!=0)
+    {
+        
+        int bit = n&1;
+        cout<<n<<" "<<bit<<" ";
+        ans = (bit * pow(10,i) ) + ans;
+        n = n>>1;
+        i++;
+    }
+    cout<<ans<<endl;
+    
+    return 0;
+}
