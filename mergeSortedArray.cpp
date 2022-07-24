651dd8c1401a5f8a82d9320c75627ee8766f6158
Anurag Choudhary <panigale215@gmail.com>
2022-07-24 18:28:17 +0530

    mergeSort

---
+++ mergeSortedArray.cpp
@@ -0,0 +1,21 @@
+#inclde<iostream>
+#include<array>
+using namespace std;
+
+array merge(int arr1[], int arr[2])
+{
+    
+}
+
+
+
+int main()
+{
+    int arr1[5] = {1,3,5,7,9};
+    int arr2[3] = {2,4,6};
+    int arr3[8] = {0};
+    arr3 =  merge(arr1 , arr2);
+
+
+    return 0;
+}
