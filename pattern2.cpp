ab083bb6dafb0b62ec0b1563c88697cfc66f742c
Anurag Choudhary <panigale215@gmail.com>
2022-07-31 01:32:09 +0530

    pattern2

---
+++ pattern2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j,k;
+    for(i=1;i<=5;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
