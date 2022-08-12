cf3a1b8cadb8c09ed836041cca15c6cd797de331
Anurag Choudhary <panigale215@gmail.com>
2022-08-12 06:11:50 +0530

    python

---
+++ odict.py
@@ -0,0 +1,7 @@
+from collections import OrderedDict
+d = OrderedDict()
+d[1] = 'a'
+d[3] = 'c'
+d[2] = 'b'
+d[4] = 'd'
+print(d)
---
+++ wipro.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<map>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    map<int,int> m;
+    int i,j;
+    int maxi=0,mini=0;
+    int arr[3];
+    cout<<"enter 3 numbers : ";
+    for(i=0;i<3;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    vector<int> v;
+    while
+    return 0;
+}
