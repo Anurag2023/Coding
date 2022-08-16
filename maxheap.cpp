bd505e6defc59ac5cfdc9da1d30cda5f18a8328d
Anurag Choudhary <panigale215@gmail.com>
2022-08-17 01:19:03 +0530

    min heap added

---
+++ maxheap.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{   
+    int i;
+    vector<int> arr(6);
+    arr = {5,3,8,4,6,7};
+    priority_queue<int,vector<int>,greater<int>> minh(arr.begin(),arr.end());//min heap
+    priority_queue<int>maxh(arr.begin(),arr.end()); // max heap
+    
+    for(i=0;i<6;i++)
+    {
+        cout<<minh.top()<<" ";  // min heap
+        minh.pop();
+    }
+    cout<<endl;
+    for(i=0;i<arr.size();i++)
+    {
+        cout<<maxh.top()<<" ";  // max heap
+        maxh.pop();
+    }
+
+
+    return 0;
+}
