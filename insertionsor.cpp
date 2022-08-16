02b5db1351717cf77703210b1cc31bb89167bf05
Anurag Choudhary <panigale215@gmail.com>
2022-08-16 08:47:13 +0530

    whileloop

--- insertionsor.cpp
+++ insertionsor.cpp
@@ -1,41 +1,35 @@
 #include<iostream>
-#include<vector>
-#include<array>
-#include<algorithm>
 using namespace std;
-void shift(int arr[] , int i , int j )
-{
-        int temp = arr[i];        
-        while(i!=j)
-        {
-            arr[i] = arr[i-1];
-            i--; 
-        }
-        arr[j] = temp;
-}
 int main()
 {
-    int a,b,c,i,j;
-   // vector<int> arr;
-    int n=0;
-    cout<<"enter size : ";
+    int i,j;
+    cout<<"enter the size of the array : ";
+    int n;
     cin>>n;
+    int temp;
     int arr[n];
-    cout<<"enter elements : ";
-    for(i=0;i<n;i++)
+    cout<<"enter the elements : ";
+    for(i=0;i<n;)
     {
-        cin>>arr[i];
+        cin>>arr[i++];
     }
     
     for(i=1;i<n;i++)
     {
-        j=i-1;        
-        while(arr[j]>arr[i] && j>-1)
-            j--;
-        shift(arr,i,j+1);
+        temp = arr[i];
+        j = i-1;
+        {
+            while(arr[j]>temp && j>-1)
+            {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
     }
-
-    for(auto k : arr)
-        cout<<k<<" ";
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
