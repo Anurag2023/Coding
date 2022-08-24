360d3401f80e455c6bd867ac83b54776521d4891
Anurag Choudhary <panigale215@gmail.com>
2022-08-24 07:38:26 +0530

    recursion

--- recc.cpp
+++ recc.cpp
@@ -1,21 +1,33 @@
 #include<iostream>
+#include<vector>
+#include<vector>
+#include<map>
+#include<queue>
+#include<string>
 using namespace std;
-
-void rec(int n)
-{
-    if(n==1)
-        {
-            cout<<n<<" ";
-            return;
-        }
-    //cout<<" B "<<n<<" ";
-    rec(n-1);
-    cout<< n<<" ";
-    
-}
-
 int main()
 {
-    rec(10);
+    vector<int>vec;
+    vec = {4,3,6,8,3,5,4,4,5};
+   // cout<<vec[3];
+    map<int,int>m;
+    for(int i=0;i<vec.size();i++)
+    {
+        m[vec[i]]++;
+    }
+    for(auto i : m)
+    {
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+    map<int,int>ma;
+    for(auto i : m)
+    {
+        ma[i.second] = i.first;
+    }
+    for(auto i : ma)
+    {
+        cout<<"  ma "<<i.first<<" "<<i.second<<endl;
+    }
+
     return 0;
 }
