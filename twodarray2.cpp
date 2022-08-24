87cd2836b49f54d1c94b1865ed31dcad311ea80e
Anurag Choudhary <panigale215@gmail.com>
2022-08-24 15:53:07 +0530

    twoDArray

---
+++ twodarray2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int nums[3][3]={1,2,3,4,5,6,7,8,9};
+	int i,j=0;
+	for(i=0;i<3;i++)
+	{
+		if(j==0){
+			while(j<3)
+			{
+				cout<<nums[j++][i]<<" ";
+			}
+		j-=2;
+        }
+        
+		//cout<<endl;
+		else
+		{
+			while(j>=0)
+			{
+				cout<<nums[j--][i]<<" ";
+			}
+		}
+        j++;
+		cout<<endl;
+	}
+
+
+	return 0;
+}
