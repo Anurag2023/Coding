65fdbc8df95b728612e3dd8d7c2462d0b2071817
Anurag Choudhary <panigale215@gmail.com>
2022-08-24 07:38:03 +0530

    bis

---
+++ stdc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<vector>
+#include<map>
+#include<queue>
+#include<string>
+
+using namespace std;
+int main()
+{
+    cout<<"hello";
+    return 0;
+}
