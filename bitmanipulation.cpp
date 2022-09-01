7f293f28dd672c51a71c407279d605abdd79523b
Anurag Choudhary <panigale215@gmail.com>
2022-09-01 17:50:17 +0530

    bitmanipulation

---
+++ bitmanipulation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+#include<numeric>
+using namespace std;
+int main(){
+    int a=INT_MAX;
+    //cout<<"enter a no : ";
+    //cin>>a;
+    while(a>0)
+    {
+        cout<<a<<" ";
+        a = (a&(a-1));
+    }
+    cout<<a<<" ";
+    return 0;
+}
